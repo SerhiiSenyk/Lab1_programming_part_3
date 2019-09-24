fe93ae63ceef1c969298c16e8a2d9667db9529d7
SerhiiSenyk <email>
2019-09-24 23:31:13 +0300

    class Refrigerator,contain 4 fields and 8 methods

---
+++ Refrigerator.h
@@ -0,0 +1,34 @@
+﻿#ifndef REFRIGERATOR_H
+#define REFRIGERATOR_H
+#include <string>
+using namespace std;
+
+
+
+/*Створити клас “холодильна камера” котрий містить поля :
+-фірма - виробник
+- Загальний об'єм, л
+- Вага(кг)
+- споживана потужність(Ватт)*/
+class Refrigerator
+{
+	private:
+		string firmManufacturer;
+		double totalVolume;
+		double weight;
+		double power;
+	public:
+		void setFirmManufacturer(string firmManufacturer);
+		string getFirmManufacturer() const;
+		void setTotalVolume(double totalVolume);
+		double getTotalVolume() const;
+		void setWeight(double weight);
+		double getWeight() const;
+		void setPower(double power);
+		double getPower() const;
+		Refrigerator(string firmManufacturer, double totalVolume,
+					double weight, double power);
+		Refrigerator();
+		~Refrigerator();
+};
+#endif //!REFRIGERATOR_H
